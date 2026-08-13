37b5637fb795cff8870f61c1536961caf8b6d208
Rahman Karim <rahmankarim2468@gmail.com>
2026-08-13 15:59:08 +0500

    Time: 0 ms (100%), Space: 53.6 MB (53.17%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+    let lastWord = s.trim().split(" ");
+
+    return lastWord[lastWord.length -1].length;
+    
+};
