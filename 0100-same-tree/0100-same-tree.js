a40b400784a87c74847b04d97b50878ab6eaa887
Rahman Karim <rahmankarim2468@gmail.com>
2026-08-17 02:23:04 +0500

    Time: 0 ms (100%), Space: 53.7 MB (71.22%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.js
@@ -0,0 +1,22 @@
+var isSameTree = function(p, q) {
+
+    function check(node1, node2) {
+
+        if (node1 === null && node2 === null) {
+            return true;
+        }
+
+        if (node1 === null || node2 === null) {
+            return false;
+        }
+
+        if (node1.val !== node2.val) {
+            return false;
+        }
+
+        return check(node1.left, node2.left) &&
+               check(node1.right, node2.right);
+    }
+
+    return check(p, q);
+};
