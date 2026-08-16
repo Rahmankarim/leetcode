2da1780b46c47df33a4c50d466f63e96bbe480f5
Rahman Karim <rahmankarim2468@gmail.com>
2026-08-16 14:16:12 +0500

    Time: 0 ms (100%), Space: 53.5 MB (59.64%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let a = 1;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        let temp = a + b;
+        a = b;
+        b = temp;
+    }
+
+    return b;
+};
