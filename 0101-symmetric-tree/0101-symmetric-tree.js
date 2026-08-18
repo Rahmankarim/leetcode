6c411d8b03017a987b86fdf5c687f2ac5b9e2786
Rahman Karim <rahmankarim2468@gmail.com>
2026-08-18 23:06:10 +0500

    Time: 1 ms (12.2%), Space: 58 MB (17.23%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    
+
+
+    function check(node1, node2)
+    {
+        if(node1 === null && node2 === null)
+        {
+
+            return true;
+
+        }
+
+        if(node1 === null || node2 ===null)
+        {
+            return false;
+        }
+       if (node1.val !== node2.val) {
+            return false;
+        }
+
+ if (root === null) {
+        return true;
+    }
+
+        return check(node1.left, node2.right) && check(node1.right, node2.left);
+
+
+
+    }
+
+
+    return check(root.left,root.right);
+};
