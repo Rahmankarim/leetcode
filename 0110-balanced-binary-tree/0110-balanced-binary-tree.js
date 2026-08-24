b45dfbe19792275eeff9e6f96ba4cf10328ada83
Rahman Karim <rahmankarim2468@gmail.com>
2026-08-25 02:18:54 +0500

    Time: 1 ms (83.77%), Space: 60.1 MB (50.95%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -6,24 +6,18 @@ var isBalanced = function(root) {
 
         let leftHeight = checkHeight(root.left);
 
-        // Left side is already unbalanced
         if (leftHeight === -1) return -1;
 
-        // Get right height
         let rightHeight = checkHeight(root.right);
 
-        // Right side is already unbalanced
-        if (rightHeight === -1) return -1;
+     if (rightHeight === -1) return -1;
 
-        // Current node is unbalanced
         if (Math.abs(leftHeight - rightHeight) > 1) {
             return -1;
         }
 
-        // Return height
         return 1 + Math.max(leftHeight, rightHeight);
     }
 
-    // -1 means unbalanced
     return checkHeight(root) !== -1;
 };
